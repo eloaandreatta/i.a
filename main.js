8565c6af7468e1fee08503ec2b91aa21466a4ad3
Eloá <111518085+eloaandreatta@users.noreply.github.com>
2024-08-13 11:51:18 -0300

    Update main.js

--- main.js
+++ main.js
@@ -9,11 +9,11 @@ const perguntas = [
         enunciado: "Como a música de um artista pode promover o respeito e a conscientização sobre a inclusão e a caça ilegal que leva à extinção de animais?" ,
         alternativas: [
             {
-               texto: "Sim",
+               texto: "Ele  pode abordar os tópicos em suas composições assim “educando” quem escutar a música.",
                afirmação: "afirmação"
             },
             {
-                texto: "Não",
+                texto: "O artista tentando abordar todos esses tópicos, pode diluir a mensagem fazendo com que nenhum do tópicos recebam atenção e a missão principal da música, falhe.",
                 afirmação: "afirmação"
             }
         ]
@@ -22,20 +22,20 @@ const perguntas = [
         enunciado: "A acessibilidade, a diversidade musical e o crescimento urbano desenfreado impactam a inclusão social e a preservação de espécie ameaçadas?" ,
         alternativas: [
             {
-               texto: "Sim",
+               texto: "A cultura dos povos indígenas é rica e caracteriza-se pela intensa relação com a natureza.A questão da procriação é natural e vista como comum",
                afirmação: "afirmação"
             },
             {
-                texto: "Não",
+                texto: "Apoiam as medidas já tomadas.",
                 afirmação: "afirmação"
             }
         ]
     },
     { 
-        enunciado: "Como a tecnologia assistiva está sendo aplicada para promover inclusão, a música como instrumento de conscientização e a luta contra o desmatamento para evitar a extinção de animais?" ,
+        enunciado: "A tecnologia assistiva está sendo aplicada para promover inclusão, a música como instrumento de conscientização e a luta contra o desmatamento para evitar a extinção de animais?" ,
     alternativas: [
         {
-           texto: "Sim",
+           texto: "Uso das redes sociais para apoio e divulgação das ações.",
            afirmação: "afirmação"
         },
         {
